888162ab56b9a7a8c9709dcabb10806c34e8b9c6
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2025-01-18 14:53:25 +0800

    Revamp

---
+++ Enlistment.js/Enlistment.js
@@ -0,0 +1,108 @@
+
+export default class Enlistment {
+
+    constructor(container, courses, maxEnrollment = 9) {
+        
+        this.maxEnrollment = 9;
+
+        this.onEnlist = null;
+        this.onDrop = null;
+        this.onFull = null;
+
+        this.root = document.querySelector(container);
+
+        this.courses = courses;
+        this.enlistedCourses = [];
+
+        this.enlistedCoursesDiv = document.createElement('div');        
+        this.courseOfferingDiv = document.createElement('div');
+
+        this.root.appendChild(this.courseOfferingDiv);
+        this.root.appendChild(this.enlistedCoursesDiv);
+
+        this.renderCourses();
+        
+    }
+
+    getTotalUnits() {
+        let units = 0;
+
+        for (const courseId of this.enlistedCourses) {
+            const course = this.courses.find(course => course.id === courseId);
+            units += course.units;
+        }
+
+        return units;
+    }
+
+    renderCourses() {
+        this.courseOfferingDiv.innerHTML = '<h2>Course Offerings</h2>';
+        this.enlistedCoursesDiv.innerHTML = '<h2>Enlisted Course</h2>';
+
+        this.courses.forEach(course => {
+            if (!this.enlistedCourses.includes(course.id)) {
+                this.renderCourseItem(this.courseOfferingDiv, course, 'Enlist', () => this.enlist(course.id));
+            }
+        });
+
+        this.enlistedCourses.forEach(courseId => {
+            const course = this.courses.find(c => c.id === courseId);
+            if (course) {
+                this.renderCourseItem(this.enlistedCoursesDiv, course, 'Drop', () => this.drop(course.id));
+            }
+        });
+    }
+    
+    renderCourseItem(container, course, actionText, actionCallback) {
+        const courseCard = document.createElement('div');
+        const { id, name, units, schedule } = course;
+        const { code, section } = this.parseCourseId(id);
+        
+        courseCard.id = id;
+        courseCard.className = 'course-item';
+        courseCard.innerHTML = `
+            <h4>[${code} | ${section}] ${name}</h4>
+            <p>${units} units </p>
+            <p>${schedule}</p>
+        `;
+
+        const actionButton = document.createElement('button');
+        actionButton.addEventListener('click', actionCallback);
+        actionButton.id = `btn-${id}`
+        actionButton.innerText = actionText;
+        courseCard.appendChild(actionButton);
+
+        container.appendChild(courseCard);
+    }
+
+    parseCourseId(id) {
+        const [ code, section ] = id.split('-');
+
+        return {
+            code: code,
+            section: section
+        };
+    }
+
+    enlist(courseId) {
+        console.log(courseId + ' is enlisted ');
+        if (!this.enlistedCourses.includes(courseId)) {
+            this.enlistedCourses.push(courseId);
+            this.renderCourses();
+        }
+
+        if (this.onEnlist) {
+            this.onEnlist(courseId);
+        }
+    }
+
+    drop(courseId) {
+        this.enlistedCourses = this.enlistedCourses.filter(id => id !== courseId);
+        this.renderCourses();
+
+        if (this.onDrop) {
+            this.onDrop(courseId);
+        }
+    }
+
+};
--- index.js
+++ index.js
@@ -1,40 +1,69 @@
-import { SkillTree } from './SkillTree.js/SkillTree.mjs'
+import Enlistment from './Enlistment.js/Enlistment.js'
 
-const skillTree = new SkillTree('#root', {
-    id: 'web-development',
-    name: 'Web Development',
-    children: [
-        {
-            id: 'html',
-            name: "HTML Basics",
-            children: [
-                { id: "semantic", name: "Semantic HTML", children: [] },
-                { id: "forms", name: "HTML Forms", children: [] }
-            ]
-        }
-    ]
-});
+const root = document.getElementById('root');
+const courses = [
+    /**
+     * TODO: Populate this list with the following course offerings:
+     * 
+     * - CCPROG1 (Section S15)
+     * - CCPROG1 (Section S16)
+     * - CCDSALG (Section S11)
+     * - CCDSALG (Section S12)
+     */
 
-skillTree.addSkill({
-    id: 'css',
-    name: 'CSS Fundamentals',
-    prerequisite: 'web-development',
-    children: []
-})
+    {
+        id: 'CCPROG1-S15',
+        name: 'Programming 1',
+        units: 3,
+        schedule: 'MH 9:00 AM - 10:30 AM'
+    },
+    {
+        id: 'CCPROG1-S16',
+        name: 'Programming 1',
+        units: 3,
+        schedule: 'TF 1:00 PM - 3:00 PM'
+    },
+    {
+        id: 'CCDSALG-S11',
+        name: 'Data Structures and Algorithms',
+        units: 3,
+        schedule: 'MH 10:30 AM - 12:00 PM'
+    },
+    {
+        id: 'CCDSALG-S12',
+        name: 'Data Structures and Algorithms',
+        units: 3,
+        schedule: 'TF 3:00 PM - 5:00 PM'
+    },
+    {
+        id: 'CCAPDEV-S18',
+        name: 'Web Application Development',
+        units: 3,
+        schedule: 'WS 1:00 PM - 2:30 PM'
+    }
+];
 
-skillTree.addSkill({
-    id: 'css-selectors',
-    name: 'CSS Selectors',
-    prerequisite: 'css'
-});
+const courseRender = new Enlistment('#root', courses);
 
-skillTree.addSkill({
-    id: 'javascript',
-    name: 'JavaScript Basics',
-    prerequisite: 'web-development'
-})
+courseRender.onEnlist = (courseId) => {
+    /**
+     * TODO: Create a notification modal that provides feedback
+     */
+}
 
-skillTree.render();
+courseRender.onDrop = (courseId) => {
+    /**
+     * TODO: Create a notification modal that provides feedback
+     */
+}
 
-// console.log(skillTree.traverseSkillTree(skillTree.root, 'css'));
+courseRender.onFull = () => {
+    /**
+     * BONUS TODO: Create an error modal that provides feedback
+     */
+}
 
+/**
+ * BONUS TODO: Style the application accordingly to be more visually appealing.
+ * HINT: Go to `index.html`.
+ */
